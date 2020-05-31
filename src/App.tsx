de3b27220f05be3a495d630dca19f04cb64f81e0
vladzyabs <59650523+vladzyabs@users.noreply.github.com>
2020-05-31 22:33:22 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -33,12 +33,15 @@ const fullName = {
 }
 
 let messageDate = {
-    id: 1,
-    userName: 'Elliot',
-    photo: 'https://i2.wp.com/itc.ua/wp-content/uploads/2017/12/dims-11.jpg?fit=1524%2C1080&quality=100&strip=all&ssl=1',
-    message: 'A bug is never just a mistake. It represents something bigger. An error of thinking that makes you who you are.',
-    time: function() {
-      let date = new Date() 
-      return `${date.getHours()}:${date.getMinutes()}`
-    },
+  id: 1,
+  userName: 'Elliot',
+  photo: 'https://i2.wp.com/itc.ua/wp-content/uploads/2017/12/dims-11.jpg?fit=1524%2C1080&quality=100&strip=all&ssl=1',
+  message: 'A bug is never just a mistake. It represents something bigger. An error of thinking that makes you who you are.',
+  time: function () {
+    let date = new Date()
+    function checkTime(i: any) {
+      return (i < 10) ? i = "0" + i : i
+    }
+    return `${checkTime(date.getHours())}:${checkTime(date.getMinutes())}`
+  },
 }
