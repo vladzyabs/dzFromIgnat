f8e3ec4d9d63856385080782590f7b13e7b61950
vladzyabs <vladzyaba@gmail.com>
2020-06-28 22:23:32 +0300

    Updates

---
+++ precache-manifest.9a52a69fbda376296bfb5d85848840ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b35e4c1d850362711c71526459a4854",
+    "url": "/dzFromIgnat/index.html"
+  },
+  {
+    "revision": "842e463d4516176cb9f8",
+    "url": "/dzFromIgnat/static/css/main.c5106d8e.chunk.css"
+  },
+  {
+    "revision": "e4a153ffd10cbb21136c",
+    "url": "/dzFromIgnat/static/js/2.bcd3e820.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dzFromIgnat/static/js/2.bcd3e820.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "842e463d4516176cb9f8",
+    "url": "/dzFromIgnat/static/js/main.ba256c28.chunk.js"
+  },
+  {
+    "revision": "d3574620ce89646e5a0f",
+    "url": "/dzFromIgnat/static/js/runtime-main.fb627a3f.js"
+  }
+]);
