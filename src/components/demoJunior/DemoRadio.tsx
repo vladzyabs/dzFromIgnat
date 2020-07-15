9c2492fa56aa29699351e29e81799c4c06f492b3
vladzyabs <vladzyaba@gmail.com>
2020-07-15 17:13:27 +0300

    refactoring radio

--- src/components/demoJunior/DemoRadio.tsx
+++ src/components/demoJunior/DemoRadio.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import style from "./Demo.module.scss";
 import Radio from "../common/Radio/Radio";
 
 function DemoSelect() {
 
     const radioValue = ['radio 1', 'radio 2', 'radio 3'];
-    const onSelectRadio = (value: string) => {alert(value)}
+    const [value, setValue] = useState(radioValue[0])
+    const onSelectRadio = (value: string) => {
+        setValue(value)
+        alert(value)
+    }
 
     return (
         <div className={style.demoEditSpan}>
             <h3>dz : 7</h3>
-            <Radio radioName={'demo'} radioValue={radioValue} onRadioChange={onSelectRadio} radioTitle={'Demo radio'}/>
+            <Radio radioName={'demo'} value={value} radioValue={radioValue} onRadioChange={onSelectRadio} radioTitle={'Demo radio'}/>
         </div>
     )
 }
