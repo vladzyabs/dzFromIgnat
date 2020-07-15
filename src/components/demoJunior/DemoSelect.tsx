88e4df8c7d7b443cdab84cbb602a08c4bdae5055
vladzyabs <vladzyaba@gmail.com>
2020-07-15 16:43:35 +0300

    refactoring select

--- src/components/demoJunior/DemoSelect.tsx
+++ src/components/demoJunior/DemoSelect.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import style from "./Demo.module.scss";
 import Select from "../common/Select/Select";
 
 function DemoSelect() {
 
     const selectValue = ['1 option', '2 option', '3 option'];
-    const onSelectItem = (item: string) => alert(item)
+    const [value, setValue] = useState(selectValue[0])
+    const onSelectItem = (item: string) => {
+        setValue(item)
+        alert(item)
+    }
 
     return (
         <div className={style.demoEditSpan}>
             <h3>dz : 7</h3>
-            <Select selectValue={selectValue} placeholderOption={'Demo select'} onSelectChange={onSelectItem}/>
+            <Select selectValue={selectValue} value={value} onSelectChange={onSelectItem}/>
         </div>
     )
 }
